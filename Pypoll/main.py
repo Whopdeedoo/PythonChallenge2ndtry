044dce4062da9fc552b37f9f40a17c40551ce830
Whopdeedoo <51856857+Whopdeedoo@users.noreply.github.com>
2019-07-06 21:50:30 -0400

    Update main.py

---
+++ Pypoll/main.py
@@ -0,0 +1,21 @@
+import os
+import csv
+
+csvpath = os.path.join ('Pypoll', 'Resources', 'election_data.csv')
+
+# election_data.csv (Pypoll/Resources/budget_data.csv)
+title = election_data
+election_data = {'Voter', 'ID', 'County', 'Candidate'}
+Voter = int(election_data[1])
+Candidate = ['Khan', 'Correy', 'Li', 'OTooley']
+#def print_percentages(candidate):
+for row in csv_reader
+     if 
+    
+#total_votes 
+     print(len('Voter'))
+#percentage of votes for each candidate
+#total number of votes for each candidate
+for x in candidate:
+     count(
+#winner based on election
